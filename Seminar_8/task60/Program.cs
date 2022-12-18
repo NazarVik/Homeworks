23d00083f612dc355bee76388754fc4ce26b80a9
NazarVik <Nazarov_dev@icloud.com>
2022-12-18 09:37:41 +0300

    task 60 ready

---
+++ Seminar_8/task60/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого 
+// элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] FillArray(int x, int y, int z)
+{
+    int[,,] arr = new int[x, y, z];
+    int couter = 10;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                arr[i, j, k] = couter;
+                couter += 5;
+            }
+        }
+    }
+    return arr;
+}
+
+void ShowArray(int[,,] arr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                System.Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+int[,,] matrix = FillArray(2, 2, 2);
+ShowArray(matrix);
+System.Console.WriteLine();
+
